b3ab12a4a9d5f6e0628758090c0676783d87647b
huangzt <huangzt@linkstec.com>
2018-01-22 09:10:30 +0800

    ws test

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
-"use strict";
+const express = require('express');
+const http = require('http');
+const url = require('url');
+const WebSocket = require('ws');
 
-const express = require("express");
-
-// Constants
-const PORT = 8080;
-const HOST = "0.0.0.0";
-
-// App
 const app = express();
-app.get("/", (req, res) => {
-    res.send("Hello world\n");
+
+// app.use(function (req, res) {
+//     res.send({ msg: "hello" });
+// });
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+// app.get("/", (req, res) => {
+//     res.send("Hello websocket");
+// });
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function connection(ws, req) {
+    console.log('a user connected');
+    const location = url.parse(req.url, true);
+    // You might use location.query.access_token to authenticate or share sessions
+    // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+        ws.send('received');
+    });
+
+    ws.on('close', function close(message) {
+        console.log('closed');
+    });
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+server.listen(8080, function listening() {
+    console.log('Listening on %d', server.address().port);
+});
